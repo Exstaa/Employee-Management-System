f6f3205e9ac604cc42c73132d37f29b8de8577b3
Exstaa <exstagithub@gmail.com>
2024-08-29 20:01:40 +0400

    main class.

---
+++ src/main/java/az/hafizrzazade/employeemanagement/EmployeeManagementMainClass.java
@@ -0,0 +1,11 @@
+package az.hafizrzazade.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementMainClass {
+    public static void main(String[] args) {
+    	SpringApplication.run(EmployeeManagementMainClass.class, args);
+    }
+}
